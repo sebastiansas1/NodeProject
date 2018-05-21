daa470337205fbf296103816bcfa8e5cbf32865a
Andreea Bertea <acsbertea1@sheffield.ac.uk>
2018-05-21 18:10:35 +0100

    Andreea - fixed ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+  console.log("document loaded");
   $('.delete-restaurant').on('click', function(e){
+    window.console.log("delete button clicked");
     $target = $(e.target);
     const id = $target.attr('data-id');
     $.ajax({
@@ -11,6 +13,34 @@ $(document).ready(function(){
       },
       error: function(err){
         console.log(err);
+      }
+    });
+  });
+
+  $('.edit-restaurant').on('click', function(e){
+    window.console.log("edit button clicked");
+    $target = $(e.target);
+    const id = $target.attr('data-id');
+    var name = document.getElementById('name').value;
+    console.log("name" + name);
+    var cousine = document.getElementById('cousine').value;
+    console.log("cousine" + cousine);
+    var description = document.getElementById('description').value;
+    console.log("description" + description);
+    $.ajax({
+      type: 'POST',
+      url: '/restaurants/edit/'+id,
+      data: {
+        'name': name,
+        'description': description,
+        'cousine': cousine
+      },
+      success: function(response){
+        console.log(response);
+        window.location.href='/';        
+      },
+      error: function(err){
+        console.log(err);     
       }
     });
   });
