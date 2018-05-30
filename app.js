c9ee3f539726f11372b34bccd58554edc916c9ad
Andreea Bertea <acsbertea1@sheffield.ac.uk>
2018-05-30 14:41:48 +0100

    finished reviews

--- app.js
+++ app.js
@@ -8,21 +8,18 @@ const session = require('express-session');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const config = require('./config/db');
-// var googleMapsClient = require('@google/maps').createClient({
-//   key: 'AIzaSyDWb2eLKaF_tyP9hsrTUZ3_CsOsXSoXXNg'
-// });
 
 
 mongoose.connect(config.database);
 let db = mongoose.connection;
 
 // Check connection
-db.once("open", function() {
+db.once("open", function () {
   console.log("Connected to MongoDB");
 });
 
 // Check for DB errors
-db.on("error", function(err) {
+db.on("error", function (err) {
   console.log(err);
 });
 
@@ -31,6 +28,7 @@ const app = express();
 
 // Bring in Models
 let Restaurant = require("./models/restaurant");
+let Review = require("./models/review");
 
 // Load View Engine
 app.set("views", path.join(__dirname, "views"));
@@ -68,25 +66,72 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Home Route
-app.get("/", function(req, res) {
-  Restaurant.find({}, function(err, restaurants) {
+app.get("/", function (req, res) {
+  var reviews = [];
+  var rest = [];
+  Restaurant.find({}, function (err, restaurants) {
     if (err) {
       console.log(err);
-    } else {
-      res.render("restaurant/index", {
-        title: "All Restaurants",
-        restaurants: restaurants
-      });
+    }
+    else {
+      Review.find({}, function(err2, reviews){
+        if(err2){
+          console.log(err2);
+
+        }
+        else{
+          res.render("restaurant/index", {
+            title: "All restaurants ",
+            restaurants: restaurants,
+            reviews: reviews
+          });
+        }
+      })
+    // rest.push(restaurants);
+    // for (var i = 0; i < rest.length; i++) {
+    //   console.log("  ");
+    //   console.log("foreach id = " + i);
+    //   console.log("rest id = " + restaurants[i]._id);
+    //   var rest_id = restaurants[i]._id;
+    //   rest.push(restaurants[i]);
+    //   Review.find({ restaurant_id: rest_id }, function (err2, review) {
+    //     if (err2) {
+    //       console.log(err2);
+    //     }
+    //     else {
+    //       for (var j = 0; j < review.length; j++) {
+    //         console.log("got review ");
+    //         console.log("review= " + review[j]);
+    //         reviews.push(review[j]);
+    //         console.log("pushing array");
+    //         console.log("array length ");
+    //         console.log(reviews.length);
+    //         console.log("--------------------------------")
+            
+    //         if(review[j] == review[review.length-1]){
+    //           console.log("last item ALLLEEERRTT" + review.length);
+             
+    //         }
+    //       };
+    //     }
+        
+  
+    //   });    
+    // };
+    
     }
   });
+
+
+  
 });
 
 //Search Route
-app.get("/search/:search_query", function(req, res) {
+app.get("/search/:search_query", function (req, res) {
   let search = req.params.search_query;
   var name = { name: new RegExp(search, "i") };
   var city = { city: new RegExp(search, "i") };
-  Restaurant.find({ $or: [name, city] }, function(err, restaurants) {
+  Restaurant.find({ $or: [name, city] }, function (err, restaurants) {
     if (err) {
       console.log(err);
     } else {
@@ -100,8 +145,8 @@ app.get("/search/:search_query", function(req, res) {
 
 // Require All Routes
 let restaurants = require("./routes/restaurants");
-let reviews = require("./routes/reviews", {mergeParams: true});
-let uploads = require("./routes/uploads", {mergeParams: true});
+let reviews = require("./routes/reviews", { mergeParams: true });
+let uploads = require("./routes/uploads", { mergeParams: true });
 let users = require("./routes/users");
 
 // Controller requires
@@ -118,6 +163,6 @@ app.get("/autocomplete/:search", autocomplete.find);
 app.use("/users", users);
 
 // Start Server
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
